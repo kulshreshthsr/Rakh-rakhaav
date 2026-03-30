e75b5b6c36f0ca6eda02205bf01a8e11d2ce5269
kulshreshthsr <kulshreshthasrivas@gmail.com>
2026-03-30 15:40:05 +0530

    Preload sales products for offline billing

--- client/src/app/sales/page.js
+++ client/src/app/sales/page.js
@@ -315,6 +315,7 @@ export default function SalesPage() {
     const deferredId = scheduleDeferred(async () => {
       setRefreshing(Boolean(cached?.sales));
       await fetchAll();
+      await fetchProducts();
       setRefreshing(false);
     });
 
